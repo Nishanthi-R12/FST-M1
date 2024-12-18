9798fc244a18b530b3ca948b21b9a04b4f070d34
Nishanthi-R12 <nishar12@in.ibm.com>
2024-12-18 22:14:06 +0530

    Add files via upload

---
+++ Java/Activities/HelloWorld.java
@@ -0,0 +1,19 @@
+
+public class HelloWorld {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Welcome");
+		int a=5;
+		int b=10;
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		System.out.println("a+b is " + (a+b));
+		if (a>b) {
+			System.out.println(a + " is higher value");
+		}
+		else 
+			System.out.println(b +" is higher value");
+				
+		}
+	}
